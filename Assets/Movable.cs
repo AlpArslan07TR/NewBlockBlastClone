8798b94e365676bb69bb35a6280ee221272de779
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2025-03-01 05:16:04 +0300

    Grid Created
    1-Grid&Tiles created

---
+++ Assets/Movable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movable : MonoBehaviour, IPointerDownHandler,IDragHandler,IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Down");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Up");
+    }
+}
