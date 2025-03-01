025ce34ec342829266fab4e88774dbfde206ff80
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2025-03-01 06:47:37 +0300

    SnapToGrid
    1-Block's can snap to grid now

--- Assets/Movable.cs
+++ Assets/Movable.cs
@@ -1,28 +1,136 @@
-﻿using UnityEngine;
+﻿using System;
+using MyGrid.Code;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Movable : MonoBehaviour, IPointerDownHandler,IDragHandler,IPointerUpHandler
+public class Movable : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    public Vector3 offset; 
+    public Vector3 offset;
+    [SerializeField] private LayerMask layerMask;
+    
+    private Vector3 _homePosition;
+    private Transform _currentMovable;
+    private GridManager _gridManager;
+
+    private void Start()
+    {
+        _currentMovable = transform.parent;
+        _homePosition = transform.position;
+        _gridManager = transform.parent.GetComponent<GridManager>();
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Down");
-        var target=Camera.main.ScreenToWorldPoint(eventData.position);
-        offset = transform.position - target;
+
+        var target = Camera.main.ScreenToWorldPoint(eventData.position);
+        offset = _currentMovable.position - target;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Drag");
-        
-        var target=Camera.main.ScreenToWorldPoint(eventData.position);
-        target+=offset;
-        target.z=0;
-        transform.position=target;
+
+
+        var target = Camera.main.ScreenToWorldPoint(eventData.position);
+        target += offset;
+        target.z = 0;
+        _currentMovable.position = target;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Up");
+        var canSnapToGrid = CanSnapToGrid();
+
+        
+
+        if (canSnapToGrid)
+        {
+            SetPositionAll();
+        }
+        else
+        {
+            SendBackHome();
+        }
+        
+    
     }
+
+    private void SetPositionAll()
+    {
+        foreach (var tile in _gridManager.Tiles)
+        {
+            if(!tile.gameObject.activeSelf)continue;
+            var myTile = (MyTile)tile;
+            myTile._Movable.SetPositionToHit();
+        }
+    }
+
+    private void SetPositionToHit()
+    {
+        var hit = Hit();
+        var target=hit.transform.position;
+        target.z = 0;
+        transform.position = target;
+        
+
+    }
+
+    private bool CanSnapToGrid()
+    {
+        var canSnapToGrid = true;
+        foreach (var tile in _gridManager.Tiles )
+        {
+            if(!tile.gameObject.activeSelf)continue;
+            
+            var myTile=(MyTile)tile;
+            var baseTile=myTile._Movable.Hit();
+            if (baseTile) continue;
+            canSnapToGrid = false;
+            break;
+
+
+        }
+
+        return canSnapToGrid;
+    }
+
+    private void BackHome()
+    {
+        transform.position=_homePosition;
+        
+    }
+
+    private void SendBackHome()
+    {
+        foreach (var tile in _gridManager.Tiles )
+        {
+            if(!tile.gameObject.activeSelf)continue;
+            var myTile=(MyTile)tile;
+            myTile._Movable.BackHome();
+        }
+    }
+
+
+    private RaycastHit2D Hit()
+    {
+        var origin=transform.position;
+        origin.z += .5f;
+        return Physics2D.Raycast(transform.position, Vector3.forward,10,layerMask);
+        
+    }
+
+    /*private void FixedUpdate()
+    {
+        var hit = Hit();
+        Debug.Log(hit ? $"hit{hit.transform.name}" : "no hit");
+        if (hit)
+        {
+            Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.forward)*1000,Color.yellow);
+            Debug.Log("hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.forward)*1000,Color.white);
+            Debug.Log("No hit");
+        }
+    }*/
 }
--- Assets/MyTile.cs
+++ Assets/MyTile.cs
@@ -1,6 +1,13 @@
-﻿using MyGrid.Code;
+﻿using System;
+using MyGrid.Code;
 
 public class MyTile : TileController
 {
+    public Movable _Movable { get;private set; }
 
+
+    private void Start()
+    {
+        _Movable=GetComponent<Movable>();
+    }
 }
