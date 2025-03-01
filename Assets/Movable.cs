f8f042074bae6a2f9fb4637814ee197e7aba02d7
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2025-03-01 05:23:19 +0300

    Movable class created
    1-Block's now dragging

--- Assets/Movable.cs
+++ Assets/Movable.cs
@@ -3,14 +3,22 @@ using UnityEngine.EventSystems;
 
 public class Movable : MonoBehaviour, IPointerDownHandler,IDragHandler,IPointerUpHandler
 {
+    public Vector3 offset; 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Down");
+        var target=Camera.main.ScreenToWorldPoint(eventData.position);
+        offset = transform.position - target;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Drag");
+        
+        var target=Camera.main.ScreenToWorldPoint(eventData.position);
+        target+=offset;
+        target.z=0;
+        transform.position=target;
     }
 
     public void OnPointerUp(PointerEventData eventData)
